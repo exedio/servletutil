f6136a248b79427799290a9cdcf4f1645a1a2854
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2012-08-20 14:07:40 +0000

    adjust package
    git-svn-id: https://svn.exedio.com/cope/servletutil/branches/bootstrap@24122 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- testsrc/com/exedio/cope/util/ServletPropertiesTest.java
+++ testsrc/com/exedio/cope/util/ServletPropertiesTest.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-package com.exedio.cops;
+package com.exedio.cope.util;
 
 import static com.exedio.cops.ServletProperties.create;
 import junit.framework.TestCase;
